ffcc76579898e4e1990ff12284090bd59b509e38
Lupesms97 <lupesms97@gmail.com>
2023-01-13 13:09:33 -0300

    Resolvendo Porblemas anteriores, com o salvamento do endereço.

---
+++ pessoas/src/main/java/attornatus/pessoas/model/repository/EnderecoRepository.java
@@ -0,0 +1,10 @@
+package attornatus.pessoas.model.repository;
+
+
+import attornatus.pessoas.model.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, String> {
+}
---
+++ pessoas/src/main/java/attornatus/pessoas/service/StartApp.java
@@ -0,0 +1,46 @@
+package attornatus.pessoas.service;
+
+import attornatus.pessoas.model.Clientes;
+import attornatus.pessoas.model.Endereco;
+import attornatus.pessoas.model.repository.ClientesRepository;
+import attornatus.pessoas.model.repository.EnderecoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartApp implements CommandLineRunner {
+    @Autowired
+    ClientesRepository clientesRepository;
+    @Autowired
+    EnderecoRepository enderecoRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Clientes cliente1 = new Clientes();
+        Endereco endereco = new Endereco();
+
+        cliente1.setNome("Felipe");
+        cliente1.setDataNascimento("29/01/1997");
+        endereco.setCep("27288080");
+        endereco.setCidade("Volta Redonda");
+        endereco.setLogadouro("Santa Cruz");
+        endereco.setNumero("10");
+
+        cliente1.setEndereco(endereco);
+
+        enderecoRepository.save(endereco);
+        clientesRepository.save(cliente1);
+
+        for(Clientes cliente:clientesRepository.findAll()) {
+            System.out.println(cliente);
+        }
+
+
+
+
+
+    }
+}
